eebb5cbed5f9d158205971db08ebf1d8d0496b81
Ace_404 <aceboy50@gmail.com>
2023-07-11 00:31:10 +0700

    Update new iQOO debloat method
    Fix adb shell blocked when running over 100  pm uninstall commands

---
+++ iqoo/login-required.bat
@@ -0,0 +1,21 @@
+adb shell pm uninstall --user 0 com.mobile.iroaming
+adb shell pm uninstall --user 0 com.vivo.videoeditor
+adb shell pm uninstall --user 0 com.vivo.livewallpaper.galaxy
+adb shell pm uninstall --user 0 com.vivo.livewallpaper.coffeetime
+adb shell pm uninstall --user 0 com.vivo.livewallpaper.coralsea
+adb shell pm uninstall --user 0 com.vivo.livewallpaper.silk
+adb shell pm uninstall --user 0 com.vivo.livewallpaper.behavioriqoo	
+adb shell pm uninstall --user 0 com.vivo.livewallpaper.LightSpeed
+adb shell pm uninstall --user 0 com.vivo.livewallpaper.Streamer
+adb shell pm uninstall --user 0 com.vivo.dream.music
+adb shell pm uninstall --user 0 com.vivo.dream.weather
+adb shell pm uninstall --user 0 com.vivo.dream.clock
+adb shell pm uninstall --user 0 com.android.bbkcalculator
+adb shell pm uninstall --user 0 com.android.bbksoundrecorder
+adb shell pm uninstall --user 0 com.vivo.compass
+adb shell pm uninstall --user 0 com.vivo.doubletimezoneclock
+adb shell pm uninstall --user 0 com.bbk.iqoo.feedback
+adb shell pm uninstall --user 0 ctrip.android.view
+
+
+
---
+++ iqoo/origin-notfound.bat
@@ -0,0 +1,35 @@
+#Only iQOO UI have it
+adb shell pm uninstall --user 0 com.vivo.livewallpaper.geometry
+adb shell pm uninstall --user 0 com.vivo.carmode
+adb shell pm uninstall --user 0 com.vivo.collage
+adb shell pm uninstall --user 0 com.sankuai.meituan
+adb shell pm uninstall --user 0 com.vivo.video.floating
+adb shell pm uninstall --user 0 com.ibimuyu.lockscreen
+adb shell pm uninstall --user 0 com.vivo.devicereg
+adb shell pm uninstall --user 0 com.vivo.flash
+adb shell pm uninstall --user 0 com.vivo.heduohao
+adb shell pm uninstall --user 0 com.rongcard.eid
+adb shell pm uninstall --user 0 com.vivo.omacp
+adb shell pm uninstall --user 0 com.vivo.mediatune
+adb shell pm uninstall --user 0 com.vivo.dr
+adb shell pm uninstall --user 0 com.vivo.car.networking
+adb shell pm uninstall --user 0 org.ifaa.aidl.manager
+adb shell pm uninstall --user 0 com.baidu.input_vivo
+adb shell pm uninstall --user 0 com.vivo.healthwidget
+
+#Only Origin 2 have it
+adb shell pm uninstall --user 0 com.vivo.carlauncher
+adb shell pm uninstall --user 0 com.vivo.accessibility
+adb shell pm uninstall --user 0 com.vivo.card
+adb shell pm uninstall --user 0 com.vivo.remoteassistant
+adb shell pm uninstall --user 0 com.vivo.smartLife
+adb shell pm uninstall --user 0 com.vivo.familycare.local 
+adb shell pm uninstall --user 0 com.yuemeng.speechsuite
+adb shell pm uninstall --user 0 com.vivo.cipherchain
+adb shell pm uninstall --user 0 com.vivo.agent
+
+#Weird
+adb shell pm uninstall --user 0 com.iflytek.speechsuite
+
+
+
