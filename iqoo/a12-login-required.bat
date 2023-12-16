c19a139e5b789b4a36e39b9fa54d3d33dbff8dd4
Ace_404 <aceboy50@gmail.com>
2023-12-17 02:20:06 +0700

    debloat: Add new method for android 12
    Especially for iQOO 3, iQOO Neo 3

---
+++ iqoo/a12-login-required.bat
@@ -0,0 +1,19 @@
+adb shell service call package 134 s16 com.mobile.iroaming i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.videoeditor i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.livewallpaper.galaxy i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.livewallpaper.coffeetime i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.livewallpaper.coralsea i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.livewallpaper.silk i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.livewallpaper.behavioriqoo	 i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.livewallpaper.LightSpeed i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.livewallpaper.Streamer i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.dream.music i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.dream.weather i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.dream.clock i32 0 i32 0
+adb shell service call package 134 s16 com.android.bbkcalculator i32 0 i32 0
+adb shell service call package 134 s16 com.android.bbksoundrecorder i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.compass i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.doubletimezoneclock i32 0 i32 0
+adb shell service call package 134 s16 com.bbk.iqoo.feedback i32 0 i32 0
+adb shell service call package 134 s16 ctrip.android.view i32 0 i32 0
+PAUSE
---
+++ iqoo/a12-origin-notfound.bat
@@ -0,0 +1,33 @@
+#Only iQOO UI have it
+adb shell service call package 134 s16 com.vivo.livewallpaper.geometry i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.carmode i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.collage i32 0 i32 0
+adb shell service call package 134 s16 com.sankuai.meituan i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.video.floating i32 0 i32 0
+adb shell service call package 134 s16 com.ibimuyu.lockscreen i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.devicereg i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.flash i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.heduohao i32 0 i32 0
+adb shell service call package 134 s16 com.rongcard.eid i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.omacp i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.mediatune i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.dr i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.car.networking i32 0 i32 0
+adb shell service call package 134 s16 org.ifaa.aidl.manager i32 0 i32 0
+adb shell service call package 134 s16 com.baidu.input_vivo i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.healthwidget i32 0 i32 0
+
+#Only Origin 2 have it
+adb shell service call package 134 s16 com.vivo.carlauncher i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.accessibility i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.card i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.remoteassistant i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.smartLife i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.familycare.local  i32 0 i32 0
+adb shell service call package 134 s16 com.yuemeng.speechsuite i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.cipherchain i32 0 i32 0
+adb shell service call package 134 s16 com.vivo.agent i32 0 i32 0
+
+#Weird
+adb shell service call package 134 s16 com.iflytek.speechsuite i32 0 i32 0
+PAUSE
